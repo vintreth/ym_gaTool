7d14d1bb579a938794d8649757d273adf8996dea
kbogdanov <kbogdanov@brand-in-trend.ru>
2016-05-12 12:54:27 +0300

    configuration

---
+++ src/main/java/ru/names/ym_gaTool/JsonConfiguration.java
@@ -0,0 +1,12 @@
+package ru.names.ym_gaTool;
+
+/**
+ * @author kbogdanov 12.05.16
+ */
+interface JsonConfiguration {
+
+    /**
+     * @return file name of the configuration file existed in the root directory
+     */
+    String getFileName();
+}
